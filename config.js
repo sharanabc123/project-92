21a5e1869a5d6aca0e148aa1256abefb901f4e0b
sharanabc123 <84074189+sharanabc123@users.noreply.github.com>
2022-04-01 17:56:18 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBzyZHT8TGHEUZfvs949H_7ObuO0sfiNfQ",
+    authDomain: "storytelling-app-5bdc2.firebaseapp.com",
+    projectId: "storytelling-app-5bdc2",
+    storageBucket: "storytelling-app-5bdc2.appspot.com",
+    messagingSenderId: "164340964317",
+    appId: "1:164340964317:web:9b36247ab7855f5e537434"
+  };
